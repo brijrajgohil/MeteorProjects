410a5c80798344e6b73ce78ad74389d50c2d16fe
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-03-22 19:23:47 +0530

    Add Checked and Delete Functionality

--- todos/todos.js
+++ todos/todos.js
@@ -4,11 +4,33 @@ if (Meteor.isClient) {
     //Template helpers
     Template.main.helpers({
         todos: function(){
-            return Todos.find();
+            return Todos.find({}, {sort: {createdAt: -1}});
         }
     });
-}
 
-if (Meteor.isServer) {
+    Template.main.events({
+        "submit .new-todo": function(event) {
+            var text = event.target.text.value;
+            Todos.insert({
+                text: text,
+                createdAt: new Date()
+            });
 
+            //Clear Form
+            event.target.text.value='';
+
+            //Prevent Submit
+            return false
+        },
+
+        "click .toggle-checked": function() {
+            Todos.update(this._id, {$set:{checked: ! this.checked}});
+        },
+
+        "click .delete-todo": function() {
+            if(confirm("Are you sure?")) {
+                Todos.remove(this._id);
+            }
+        }
+    });
 }
