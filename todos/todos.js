b9e283158a13e1c66fa494b51da00b83f9e7074d
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-03-24 18:00:04 +0530

    Add Meteor.methods

--- todos/todos.js
+++ todos/todos.js
@@ -11,13 +11,7 @@ if (Meteor.isClient) {
     Template.main.events({
         "submit .new-todo": function(event) {
             var text = event.target.text.value;
-            Todos.insert({
-                text: text,
-                createdAt: new Date(),
-                userID: Meteor.userID,
-                username: Meteor.user().username
-            });
-
+            Meteor.call('addTodo', text);
             //Clear Form
             event.target.text.value='';
 
@@ -26,12 +20,12 @@ if (Meteor.isClient) {
         },
 
         "click .toggle-checked": function() {
-            Todos.update(this._id, {$set:{checked: ! this.checked}});
+            Meteor.call('setChecked', this._id, !this.checked);
         },
 
         "click .delete-todo": function() {
             if(confirm("Are you sure?")) {
-                Todos.remove(this._id);
+                Meteor.call('deleteTodo', this._id);
             }
         }
     });
@@ -40,3 +34,25 @@ if (Meteor.isClient) {
         passwordSignupFields: "USERNAME_ONLY"
     });
 }
+
+//Meteor Methods
+Meteor.methods({
+    addTodo: function(text) {
+        if(!Meteor.userID()) {
+            throw new Meteor.error('not-authorised');
+        }
+        Todos.insert({
+            text: text,
+            createdAt: new Date(),
+            userID: Meteor.userID,
+            username: Meteor.user().username
+        });
+    },
+
+    deleteTodo: function(todoId) {
+        Todos.remove(todoId);
+    },
+    setChecked: function(todoId, setChecked) {
+        Todos.update(todoId, {$set:{checked: setChecked}});
+    }
+});
