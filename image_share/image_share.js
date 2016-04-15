abaa50d235edf13f2a756866fc8644e4f6405682
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-04-16 00:24:45 +0530

    Send data from JS to template

--- image_share/image_share.js
+++ image_share/image_share.js
@@ -1,5 +1,22 @@
 if (Meteor.isClient) {
     console.log("I am the Client");
+    var img_data = [
+        {
+            img_src: "2.jpg",
+            img_alt: "my picture"
+        },
+        {
+            img_src: "3.jpg",
+            img_alt: "nature"
+        },
+        {
+            img_src: "1.jpg",
+            img_alt: "Green"
+        }
+    ];
+
+    Template.images.helpers({images:img_data});
+
 }
 
 if (Meteor.isServer) {
