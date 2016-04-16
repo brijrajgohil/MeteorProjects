9ad24acfe43c33744925fc96ad622b9eeec97a2f
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-04-16 15:11:50 +0530

    Add Modal to add new images

--- image_share/image_share.js
+++ image_share/image_share.js
@@ -3,7 +3,7 @@ console.log(Images.find().count());
 
 if (Meteor.isClient) {
     Template.images.helpers({images:
-        Images.find({}, {sort: {rating: -1}})
+        Images.find({}, {sort: {createdOn: -1, rating: -1}})
     });
 
     Template.images.events({
@@ -26,6 +26,25 @@ if (Meteor.isClient) {
             Images.upate({_id: image_id},
                         {$set: {rating:rating}
             });
+        },
+
+        'click .js-show-image-form': function(event) {
+            $("#image_add_form").modal('show');
+        }
+    });
+
+    Template.image_add_form.events({
+        'submit .js-add-form': function(event) {
+            var img_src = event.target.img_src.value;
+            var img_alt = event.target.img_alt.value;
+
+            Images.insert({
+                img_src: img_src,
+                img_alt: img_alt
+                createdOn: new Date()
+            });
+            $("#image_add_form").modal('show');
+            return false;
         }
     });
 }
