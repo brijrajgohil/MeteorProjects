4d7fb76b6beddaff5a59e06e6bc36b88c024aaa8
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-04-17 15:28:15 +0530

    Get username function

--- image_share/image_share.js
+++ image_share/image_share.js
@@ -2,14 +2,28 @@ Images = new Mongo.Collection("images");
 console.log(Images.find().count());
 
 if (Meteor.isClient) {
+
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_AND_EMAIL"
+    });
+
     Template.images.helpers({images:
-        Images.find({}, {sort: {createdOn: -1, rating: -1}})
+        Images.find({}, {sort: {createdOn: -1, rating: -1}}),
+
+        getUser: function(user_id) {
+            var user = Meteor.users.findOne({_id: user_id});
+            if(user) {
+                return user.username;
+            } else {
+                return "anonymous"
+            }
+        }
     });
 
     Template.body.helpers({
         username: function() {
             if(Meteor.use()) {
-                return Meteor.user().emails[0].address;
+                return Meteor.user().username;
             } else {
                 return "anonymous";
             }
@@ -47,13 +61,15 @@ if (Meteor.isClient) {
         'submit .js-add-form': function(event) {
             var img_src = event.target.img_src.value;
             var img_alt = event.target.img_alt.value;
-
-            Images.insert({
-                img_src: img_src,
-                img_alt: img_alt
-                createdOn: new Date()
-            });
-            $("#image_add_form").modal('show');
+            if(Meteor.user()) {
+                Images.insert({
+                    img_src: img_src,
+                    img_alt: img_alt,
+                    createdOn: new Date(),
+                    createdBy: Meteor.user()._id
+                });
+            }
+            $("#image_add_form").modal('hide');
             return false;
         }
     });
