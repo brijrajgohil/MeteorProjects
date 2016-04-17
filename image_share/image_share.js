8209fcbb9f24d10a03583bf4e181714905481339
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-04-17 23:28:50 +0530

    Infinite Scroll

--- image_share/image_share.js
+++ image_share/image_share.js
@@ -3,6 +3,21 @@ console.log(Images.find().count());
 
 if (Meteor.isClient) {
 
+    Session.set("imageLimit", 8);
+    lastScrollTop = 0;
+    $(window).scroll(function(event) {
+        //test if we near the bottom of the window
+        if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+            //where are we in the page?
+            var scrollTop = $(this).scrollTop();
+            if(scrollTop > lastScrollTop) {
+                Session.set("imageLimit", Session.get("imageLimit") + 4);
+            }
+            lastScrollTop = scrollTop;
+        }
+
+    });
+
     Accounts.ui.config({
         passwordSignupFields: "USERNAME_AND_EMAIL"
     });
@@ -13,7 +28,7 @@ if (Meteor.isClient) {
                 return Images.find({createdBy: Session.get("userFilter")}, {sort: {createdOn: -1, rating: -1}});
             }
             else {
-                return Images.find({}, {sort: {createdOn: -1, rating: -1}});
+                return Images.find({}, {sort: {createdOn: -1, rating: -1}, limiy: Session.get("imageLimit")});
             }
         },
 
